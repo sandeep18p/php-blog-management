d8e7c0ef5def5d27050410d0c671df0ee0c1cf79
Sandeep P <86551476+sandeep18p@users.noreply.github.com>
2024-11-13 11:43:28 +0530

    Update db.php

--- db.php
+++ db.php
@@ -4,7 +4,7 @@ $db = 'blog_system';
 $user = 'root';
 $pass = '';
 
-// Create PDO connection
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
